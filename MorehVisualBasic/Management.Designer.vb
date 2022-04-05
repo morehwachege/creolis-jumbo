7277e5f9a15012d1ffe2a9065a808322906ae5e6
morehwachege <antony123muriithi@gmail.com>
2022-04-05 18:22:31 +0300

    project essentials

---
+++ MorehVisualBasic/Management.Designer.vb
@@ -0,0 +1,235 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Management
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtScore = New System.Windows.Forms.TextBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.txtMinScore = New System.Windows.Forms.TextBox()
+        Me.txtMaxScore = New System.Windows.Forms.TextBox()
+        Me.btnRefresh = New System.Windows.Forms.Button()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(115, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 16)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Students"
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.Location = New System.Drawing.Point(503, 409)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(84, 33)
+        Me.btnDelete.TabIndex = 2
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(102, 371)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(41, 15)
+        Me.lblName.TabIndex = 3
+        Me.lblName.Text = "Name"
+        '
+        'lblScore
+        '
+        Me.lblScore.AutoSize = True
+        Me.lblScore.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScore.Location = New System.Drawing.Point(104, 418)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(39, 15)
+        Me.lblScore.TabIndex = 4
+        Me.lblScore.Text = "Score"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(144, 371)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(155, 20)
+        Me.txtName.TabIndex = 5
+        '
+        'txtScore
+        '
+        Me.txtScore.Location = New System.Drawing.Point(159, 417)
+        Me.txtScore.Name = "txtScore"
+        Me.txtScore.Size = New System.Drawing.Size(61, 20)
+        Me.txtScore.TabIndex = 6
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(244, 415)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(55, 23)
+        Me.btnSave.TabIndex = 7
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(527, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 16)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Scores"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(516, 158)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 16)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Maximum"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(516, 93)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 16)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Minimum"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft PhagsPa", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(714, 409)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(74, 33)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "&Exit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BackgroundColor = System.Drawing.Color.DarkSlateGray
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(107, 56)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(357, 287)
+        Me.DataGridView1.TabIndex = 12
+        '
+        'txtMinScore
+        '
+        Me.txtMinScore.Location = New System.Drawing.Point(503, 125)
+        Me.txtMinScore.Name = "txtMinScore"
+        Me.txtMinScore.Size = New System.Drawing.Size(100, 20)
+        Me.txtMinScore.TabIndex = 13
+        '
+        'txtMaxScore
+        '
+        Me.txtMaxScore.Location = New System.Drawing.Point(503, 186)
+        Me.txtMaxScore.Name = "txtMaxScore"
+        Me.txtMaxScore.Size = New System.Drawing.Size(100, 20)
+        Me.txtMaxScore.TabIndex = 14
+        '
+        'btnRefresh
+        '
+        Me.btnRefresh.Location = New System.Drawing.Point(614, 409)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(75, 33)
+        Me.btnRefresh.TabIndex = 15
+        Me.btnRefresh.Text = "Refresh"
+        Me.btnRefresh.UseVisualStyleBackColor = True
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.lblTitle.Location = New System.Drawing.Point(327, 13)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(136, 20)
+        Me.lblTitle.TabIndex = 16
+        Me.lblTitle.Text = "Please Be Careful"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.btnRefresh)
+        Me.Controls.Add(Me.txtMaxScore)
+        Me.Controls.Add(Me.txtMinScore)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.txtScore)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblScore)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblScore As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtScore As TextBox
+    Friend WithEvents btnSave As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents txtMinScore As TextBox
+    Friend WithEvents txtMaxScore As TextBox
+    Friend WithEvents btnRefresh As Button
+    Friend WithEvents lblTitle As Label
+End Class
---
+++ MorehVisualBasic/Management.vb
@@ -0,0 +1,134 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Management
+
+    Public cmd As MySqlCommand
+    Public conn As New MySqlConnection
+    Public id As Integer
+    Public rd As MySqlDataReader
+    Public minStr As String
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        conn.ConnectionString = "server=localhost; database=vbapp; username=root; password=;"
+        If conn.State = ConnectionState.Open Then
+            conn.Close()
+        End If
+        conn.Open()
+        displayData()
+
+    End Sub
+
+    Public Sub displayData()
+        cmd = conn.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "select * from students"
+        cmd.ExecuteNonQuery()
+        Dim dt As New DataTable()
+        Dim da As New MySqlDataAdapter(cmd)
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
+    End Sub
+
+    Public Sub minimumScore()
+        cmd = conn.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "select min(student_grade) from students"
+        cmd.ExecuteNonQuery()
+        rd = cmd.ExecuteReader()
+        If rd.Read Then
+
+            Dim idSql As Integer = rd.GetInt32(0)
+            minStr = rd.GetString(0)
+            Dim minInt As Integer = rd.GetInt32(0)
+            txtMinScore.Text = minInt
+
+
+        End If
+        rd.Close()
+    End Sub
+
+    Public Sub maximumScore()
+        cmd = conn.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "select max(student_grade) from students"
+        cmd.ExecuteNonQuery()
+        rd = cmd.ExecuteReader()
+        If rd.Read Then
+
+            Dim idSql As Integer = rd.GetInt32(0)
+            minStr = rd.GetString(0)
+            Dim maxInt As Integer = rd.GetInt32(0)
+            txtMaxScore.Text = maxInt
+        End If
+        rd.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        cmd = conn.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "INSERT INTO students VALUES ('NULL','" + txtName.Text + "','" + txtScore.Text + "')"
+        cmd.ExecuteNonQuery()
+        txtName.Text = ""
+        txtScore.Text = ""
+        displayData()
+        minimumScore()
+        maximumScore()
+        MessageBox.Show("Data Saved Successfully")
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        id = Convert.ToInt32(DataGridView1.SelectedCells.Item(0).Value.ToString)
+        cmd = conn.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "delete from students where id=" & id & ""
+        Try
+            cmd.ExecuteNonQuery()
+        Catch ex As Exception
+            MessageBox.Show(ex, "Error")
+        End Try
+        Dim dt As New DataTable
+        Dim da As New MySqlDataAdapter(cmd)
+        da.Fill(dt)
+        Dim dr As MySqlDataReader
+        dr = cmd.ExecuteReader(CommandBehavior.CloseConnection)
+        While dr.Read
+            txtName.Text = dr.GetString(1).ToString
+            txtScore.Text = dr.GetString(2).ToString
+        End While
+    End Sub
+
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        If conn.State = ConnectionState.Open Then
+            conn.Close()
+        End If
+        conn.Open()
+
+        Try
+            id = Convert.ToInt32(DataGridView1.SelectedCells.Item(0).Value.ToString)
+            cmd = conn.CreateCommand()
+            cmd.CommandType = CommandType.Text
+            cmd.CommandText = "delete from students where id=" & id & ""
+            cmd.ExecuteNonQuery()
+            txtName.Text = ""
+            txtScore.Text = ""
+
+            displayData()
+            'MessageBox.Show("Deleted Successfully")
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+        Me.Refresh()
+    End Sub
+
+End Class
